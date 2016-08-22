3794f8237303a52931718348a1ffc55b16670326
shashankshekhars19 <shashankshekhars19@gmail.com>
2016-08-23 00:42:20 +0530

    Add files via upload

---
+++ accountServiceInstance/src/test/java/com/serviceRegister/AccountServiceInstanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.serviceRegister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountServiceInstanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
